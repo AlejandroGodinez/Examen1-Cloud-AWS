c9927c02d0cdf603a7b042c8fe0c725f5a305672
AlejandroGodinez <is714181@iteso.mx>
2021-03-19 00:21:50 -0600

    finished the service endpoint connection with backend and res send

--- routes/servicio.js
+++ routes/servicio.js
@@ -1,49 +1,41 @@
-//import router, path /servicio donde se usara IBM cloud foundry
 const router = require('express').Router()
 const path = require('path')
+const fetch = require('node-fetch');
+const { response } = require('express');
 
-//environment variables
-require('dotenv').config();
-
-//import Tone Analyzer from IBM Watson api
-const ToneAnalyzerV3 = require('ibm-watson/tone-analyzer/v3');
-const { IamAuthenticator } = require('ibm-watson/auth');
-
-const toneAnalyzer = new ToneAnalyzerV3({
-  version: '2017-09-21',
-  authenticator: new IamAuthenticator({
-    apikey: process.env.KEY,
-  }),
-  serviceUrl: process.env.URL,
-});
 
 router.route('/').get((req, res) => {
     //debe abrir de manera estatica el html index
-    res.sendFile(path.join(__dirname+'../../../src/html/index.html'));
+    res.sendFile(path.join(__dirname+'../../html/index.html'));
   })
 
 //instancia el metodo post donde en el req.body.sendText
 //recibe la cadena a evaluar
 router.route('/').post((req, res)=>{
     let textAnalyze = req.body.sendText
+    
+    fetch('http://localhost:8888/servicio', {
+        method: 'post',
+        body:    JSON.stringify({sendText: textAnalyze}),
+        headers: { 'Content-Type': 'application/json' },
+    })
+    .then(response => response.json())
+    .then(response => {
+        let insertHtml = `<h1>Tone Analysis </h1>`;
 
-    //create the headers for the tone analyzer, it will be a string/text
-    const toneParams = {
-        toneInput: { 'text': textAnalyze },
-        contentType: 'application/json',
-      };
-      
-      //this method returns a json with the tone of the text
-      toneAnalyzer.tone(toneParams)
-        .then(toneAnalysis => {
-            // res.send(JSON.stringify(toneAnalysis, null, 2));
-            //store the json in a variable
-            let jsonTone = toneAnalysis.result.document_tone;
-            res.send(jsonTone);
-        })
-        .catch(err => {
-            res.send({'error:': err});
+        response.tones.forEach(element => {
+            insertHtml += `        
+            <p><b>TONE_ID: </b> ${element.tone_id}</p>
+            <p><b>SCORE: </b> ${element.score}</p>
+            <p><b>TONE_NAME: </b>${element.tone_name}</p>
+            <br></br>
+            `
         });
+
+        res.send(insertHtml);
+    }
+        
+    );
 })
 
 module.exports = router 
